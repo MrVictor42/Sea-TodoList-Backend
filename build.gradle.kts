438bc5b3a5bd9251e3c7b062bd8f21abec5ecf56
MrVictor42 <guin-hu@hotmail.com>
2023-02-04 10:18:02 -0300

    Adicionando variaveis do postgresql no projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
 	implementation("com.auth0:java-jwt:3.19.2")
 	implementation("org.springframework.boot:spring-boot-starter-validation:2.7.0")
 	compileOnly("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure:2.6.8")
-	implementation("org.postgresql:postgresql:42.5.2")
+	implementation("org.postgresql:postgresql")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
